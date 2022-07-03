e3b7c3b474c87d76de81fe7fc56d9ae16321e0c7
jespinoza12 <jespinoza@student.neumont.edu>
2022-07-03 17:32:02 -0600

    s

--- poke-inc-frontend/src/components/homepage/homepage.js
+++ poke-inc-frontend/src/components/homepage/homepage.js
@@ -1,10 +1,11 @@
 import React from "react"
 import "./homepage.css"
 
+
 const Homepage = ({setLoginUser}) => {
     return (
         <div className="homepage">
-            <h1>Hello Homepage</h1>
+            <h1>Hello Welcome to my Home page</h1>
             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
         </div>
     )
