72e36a7b9c302f760ae8dd6c5d0f86df11dfb23e
jespinoza12 <jespinoza@student.neumont.edu>
2022-08-24 00:10:17 -0600

    s

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -12,20 +12,61 @@ const Navbar = (setLoginUser, picture, userId) => {
   picture = localStorage.getItem("picture")
   let history = useHistory()
 
-  function onClickLogout(){  
+  function onClickLogout(e){  
     e.preventDefault();
     history.push(process.env.PUBLIC_URL + "/")
+    window.location.reload()
+  }
+
+  function onClickHome(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/home")
+    window.location.reload()
+  }
+  function onClickAllDecks(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/allDecks")
+    window.location.reload()
+  }
+  function onClickAllForums(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/allForums")
+    window.location.reload()
+  }
+  function onClickCreateDeck(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/createDeck")
+    window.location.reload()
+  }
+  function onClickAllUsers(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/allUsers")
+    window.location.reload()
+  }
+  function onClickMyPage(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/myPage")
+    window.location.reload()
+  }
+  function onClickMyDecks(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/myDecks")
+    window.location.reload()
+  }
+  function onClickMyForums(e){  
+    e.preventDefault();
+    history.push(process.env.PUBLIC_URL + "/myForums")
     window.location.reload()
   }
 
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark width border sticky-nav">
       <ul class="navbar-nav me-auto btn-group m-3">
-          <button class="nav-link color-me btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/home")}>Home</button>
+          <button class="nav-link color-me btn-secondary btn" onClick={onClickHome}>Home</button>
           <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/collection")}>All Cards</button>
-          <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/allDecks")}>All Decks</button>
-          <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/allForums")}>All Forums</button>       
-          <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/allUsers")}>Users</button>
+          <button class="btn btn-secondary btn" onClick={onClickAllDecks}>All Decks</button>
+          <button class="btn btn-secondary btn" onClick={onClickAllForums}>All Forums</button>       
+          <button class="btn btn-secondary btn" onClick={onClickAllUsers}>Users</button>
         </ul>
         <ul class="navbar-nav center">
         <Dropdown>
@@ -40,17 +81,17 @@ const Navbar = (setLoginUser, picture, userId) => {
               <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/createForum")}>Create Forum</button>
             </Dropdown.Item>
             <Dropdown.Item>
-              <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/createDeck")}>Create Deck</button>
+              <button class="btn btn-secondary btn" onClick={onClickCreateDeck}>Create Deck</button>
             </Dropdown.Item>
             <Dropdown.Item>
-              <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/myDecks")}>My Decks</button>
+              <button class="btn btn-secondary btn" onClick={onClickMyDecks}>My Decks</button>
             </Dropdown.Item>
             <Dropdown.Item>
-              <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/myForums")}>My Forums</button>
+              <button class="btn btn-secondary btn" onClick={onClickMyForums}>My Forums</button>
             </Dropdown.Item>
             <NavDropdown.Divider varient="secondary" />
             <Dropdown.Item>
-              <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/myPage")}>My Profile Page</button>
+              <button class="btn btn-secondary btn" onClick={onClickMyPage}>My Profile Page</button>
             </Dropdown.Item>
             <Dropdown.Item>
               <button class="btn btn-secondary btn" onClick={()=>history.push(process.env.PUBLIC_URL + "/profile")}>Profile Settings</button>
